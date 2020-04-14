7d6edb8ef2a30d4bf00950bce4028e1fc72aeae9
Ekaterina <nataveres@gmail.com>
2020-04-14 17:40:55 +0200

    Done in class

--- app/mole.js
+++ app/mole.js
@@ -1,8 +1,7 @@
 class Mole {
-
-  timeoutId = null
-  isVisible = false
-  isWhacked = false
+  timeoutId = null;
+  isVisible = false;
+  isWhacked = false;
 
   static hideDelay() {
     return Math.floor(Math.random() * 3000) + 500;
@@ -13,12 +12,31 @@ class Mole {
   }
 
   handleClick(view) {
+    view.style.display = "none";
   }
 
   timeoutCallback(view) {
+    let delay = 0;
+    if (this.isVisible === true) {
+      view.style.display = "none";
+      delay = Mole.hideDelay();
+    }
+
+    if (this.isVisible === false) {
+      view.style.display = "initial";
+      delay = Mole.hideDelay();
+    }
+
+    this.isVisible = !this.isVisible;
+    setTimeout(this.timeoutCallback.bind(this, view), delay);
   }
 
   render() {
+    const container = document.createElement("span");
+    container.innerHTML = "Mole";
+    container.style.display = "none";
+    container.addEventListener("click", this.handleClick.bind(this, container));
+    setTimeout(this.timeoutCallback.bind(this, container), Mole.showDelay());
+    return container;
   }
-
 }
---
+++ init.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  const root = document.getElementById("root");
+  const board = new Board(Mole, 5, 5);
+  root.appendChild(board.render());
+};
